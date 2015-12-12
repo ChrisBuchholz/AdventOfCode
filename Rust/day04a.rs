782f1ad8ea0a87ab5ad2f69e0e4a6e811253f236
Chris Buchholz <chris@chrisbuchholz.me>
2015-12-13 00:52:20 +0100

    added rust solutions for day 4

---
+++ Rust/day04a.rs
@@ -0,0 +1,24 @@
+// cargo-deps: rust-crypto="0.2.34"
+
+extern crate crypto;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+fn main() {
+    let mut hasher = Md5::new();
+    let mut iter = 0;
+    let mut result = [0; 16];
+    loop {
+        hasher.input(b"yzbqklnj");
+        hasher.input(iter.to_string().as_bytes());
+        hasher.result(&mut result);
+        hasher.reset();
+        if result[..3] == [0, 0, 2] {
+            break;
+        }
+        iter += 1;
+    }
+    assert_eq!(iter, 282749);
+    println!("{}", iter);
+}
